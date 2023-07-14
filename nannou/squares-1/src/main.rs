8efbc98de2e75e96e07b78f128bf128ffad2b9cc
Filip Bontin <fbontin@users.noreply.github.com>
2023-07-14 10:17:29 +0200

    Add some squares with color depending on x, y

---
+++ nannou/squares-1/src/main.rs
@@ -0,0 +1,41 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::sketch(view).run();
+}
+
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+
+    draw.background().color(BLUE);
+
+    let rects = create_rects();
+    for r in rects {
+        draw_rect(r, &draw);
+    }
+
+    draw.to_frame(app, &frame).unwrap();
+}
+
+fn draw_rect(r: Rect, draw: &Draw) {
+    let c = rgb(r.x() / 100.0, r.y() / 700.0, 0.5);
+    draw.rect().xy(r.xy()).wh(r.wh()).color(c);
+}
+
+fn create_rect(x: i32, y: i32) -> Rect {
+    // Total 1024x768px, 16:12
+    // Let's make a bunch of 64x64px
+    let l = 64.0;
+    return Rect::from_x_y_w_h(x as f32 * l + 32.0, y as f32 * l + 32.0, l, l);
+}
+
+fn create_rects() -> Vec<Rect> {
+    const XS: std::ops::Range<i32> = -8..8;
+    const YS: std::ops::Range<i32> = -6..6;
+
+    return XS
+        .map(|x| YS.map(move |y| create_rect(x, y)))
+        .flatten()
+        .collect();
+}
+
